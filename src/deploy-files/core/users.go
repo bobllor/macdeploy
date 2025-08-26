d3c15140fd4b90493dd661cc751a6209f7bbf347
teboc <bobllor@yahoo.com>
2025-08-25 22:51:03 -0500

    fixed wrong code added in condition

--- src/deploy-files/core/users.go
+++ src/deploy-files/core/users.go
@@ -76,25 +76,25 @@ func CreateAccount(user yaml.User, adminInfo yaml.User, isAdmin bool, addChangeP
 	}
 
 	// turns out i forgot secure token access... that was rough to find out in prod
-	if addChangePassword {
-		secureTokenCmd := fmt.Sprintf("sudo sysadminctl -secureTokenOn '%s' -password '%s' -adminUser '%s' -adminPassword '%s'",
-			username, user.Password, adminInfo.User_Name, adminInfo.Password)
+	secureTokenCmd := fmt.Sprintf("sudo sysadminctl -secureTokenOn '%s' -password '%s' -adminUser '%s' -adminPassword '%s'",
+		username, user.Password, adminInfo.User_Name, adminInfo.Password)
 
-		_, err = exec.Command("bash", "-c", secureTokenCmd).Output()
-		if err != nil {
-			logger.Log(fmt.Sprintf("Error enabling token for user, manual interaction needed: %v", err), 3)
-			return false
-		}
-
-		logger.Log(fmt.Sprintf("Secure token added for %s", username), 6)
+	_, err = exec.Command("bash", "-c", secureTokenCmd).Output()
+	if err != nil {
+		logger.Log(fmt.Sprintf("Error enabling token for user, manual interaction needed: %v", err), 3)
+		return false
 	}
+
+	logger.Log(fmt.Sprintf("Secure token added for %s", username), 6)
 
 	createdLog := fmt.Sprintf("User %s created", username)
 	logger.Log(createdLog, 6)
 
-	err = moveScriptToDesktop("ChangePassword.command", username)
-	if err != nil {
-		logger.Log(fmt.Sprintf("Unexpected error moving password script: %v", err), 4)
+	if addChangePassword {
+		err = moveScriptToDesktop("ChangePassword.command", username)
+		if err != nil {
+			logger.Log(fmt.Sprintf("Unexpected error moving password script: %v", err), 4)
+		}
 	}
 
 	return true
