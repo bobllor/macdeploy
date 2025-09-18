726db6672171128232b2101e8c9f8cb8a8aee73f
bobllor <bobllor@yahoo.com>
2025-09-18 18:06:07 -0500

    moved secureToken into filevault.go, updated users to return an error instead of a boolean. moved logging outside to main function

--- src/deploy-files/core/filevault.go
+++ src/deploy-files/core/filevault.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"macos-deployment/deploy-files/logger"
 	"macos-deployment/deploy-files/scripts"
+	"macos-deployment/deploy-files/yaml"
 	"os/exec"
 	"strings"
 )
@@ -15,6 +16,7 @@ func EnableFileVault(adminUser string, adminPassword string) string {
 	// turns out if isactive == false the exit status is 1. ignoring the error here!
 	out, _ := exec.Command("bash", "-c", cmd).Output()
 
+	// instead of a boolean it must be in a string due to the subprocess.
 	fileVaultStatus := strings.TrimSpace(strings.ToLower(string(out)))
 
 	// either some fail happened or this is ran on a non-mac OS
@@ -56,4 +58,23 @@ func EnableFileVault(adminUser string, adminPassword string) string {
 	}
 
 	return ""
+}
+
+// addSecureToken adds the user to the SecureToken list for FileVault.
+// This is required on every new user.
+//
+// If successful then nil is returned, otherwise an error is thrown and manual interaction is needed.
+func addSecureToken(username string, userPassword string, adminInfo yaml.User) error {
+	// turns out i forgot secure token access... that was rough to find out in prod
+	secureTokenCmd := fmt.Sprintf("sudo sysadminctl -secureTokenOn '%s' -password '%s' -adminUser '%s' -adminPassword '%s'",
+		username, userPassword, adminInfo.User_Name, adminInfo.Password)
+
+	_, err := exec.Command("bash", "-c", secureTokenCmd).Output()
+	if err != nil {
+		logger.Log(fmt.Sprintf("Error enabling token for user, manual interaction needed: %v", err), 3)
+		return fmt.Errorf("failed to enable secure token for user %s: %v", username, err)
+	} else {
+		logger.Log(fmt.Sprintf("Secure token added for %s", username), 6)
+	}
+	return nil
 }
