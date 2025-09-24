da9c4aa978d49a1c91cf2096d114958ebcd8a54c
bobllor <bobllor@yahoo.com>
2025-09-24 12:30:31 -0500

    updated dmg logic

--- src/deploy-files/core/dmg.go
+++ src/deploy-files/core/dmg.go
@@ -36,25 +36,35 @@ func (d *Dmg) ReadDmgDirectory(dir string) ([]string, error) {
 	return dmgArray[:len(dmgArray)-1], nil
 }
 
+// MountDmgs takes an array of paths and mounts it to the disk via hdiutil.
+//
+// Upon successful completion, an array of paths to the mounted directory is returned.
 func (d *Dmg) MountDmgs(dmgPaths []string) {
 	cmd := "hdiutil attach '%s'"
+
+	mountDirectories := make([]string, 0)
 
 	for _, dmgPath := range dmgPaths {
 		d.log.Debug.Log("DMG path: %s", dmgPath)
 
 		if strings.Contains(dmgPath, ".dmg") {
 			newCmd := fmt.Sprintf(cmd, dmgPath)
-			out, err := exec.Command("bash", "-c", newCmd).Output()
 
+			d.log.Info.Log("Mounting %s", dmgPath)
 			d.log.Debug.Log("Command: %s", newCmd)
 
+			out, err := exec.Command("bash", "-c", newCmd).Output()
 			if err != nil {
 				d.log.Error.Log("failed to mount %s: %v", dmgPath, err)
+				continue
 			}
 
 			d.log.Debug.Log("Command output: %s", strings.TrimSpace(string(out)))
+			fmt.Println(strings.Split(string(out), " "))
 		}
 	}
+
+	d.log.Debug.Log("Mounted directories: %v", mountDirectories)
 }
 
 func (d *Dmg) UnmountDmgs(mountDir string, dmgPaths []string) {
