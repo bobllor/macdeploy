f53fc0f73ada5201c4a44af96719f9e583f24562
teboc <bobllor@yahoo.com>
2025-09-27 03:10:48 -0500

    updated tests

--- src/tests/users_test.go
+++ src/tests/users_test.go
@@ -53,7 +53,7 @@ func TestUserNoPassword(t *testing.T) {
 		Password: "admin",
 	}, scripts.NewScript(), logger.Log)
 
-	_, err := user.CreateAccount(userInfo, false)
+	_, err := user.CreateAccount(&userInfo, false)
 	if err == nil {
 		t.Error("expected password failure")
 	}
@@ -63,7 +63,7 @@ func TestAdminNoPassword(t *testing.T) {
 	config := &yaml.Config{}
 
 	// expects to fail due to the input terminal requirement
-	err := config.SetAdminPassword()
+	err := config.Admin.SetPassword()
 	if err == nil {
 		t.Fatal(err)
 	}
