01b7a220a0e20554ab543ff7fc1fbd40edbbf4d2
teboc <bobllor@yahoo.com>
2026-03-07 15:37:00 -0600

    added new option for apply policy, moved initialiaztion to pre run, wip

--- src/deploy-files/cmd/user.go
+++ src/deploy-files/cmd/user.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -19,10 +21,12 @@ var userCobra UserData
 var userCmd = &cobra.Command{
 	Use:   "user",
 	Short: "Create a user",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		root.initialize(true)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// have to use the root from root.go, there is an
 		// invalid memory address if using a new RootData.
-		root.initialize(true)
 		if userCobra.logvars.Verbose {
 			root.log.EnableInfoLog()
 		} else if userCobra.logvars.Debug {
@@ -31,13 +35,20 @@ var userCmd = &cobra.Command{
 
 		root.CreateLocal = true
 
-		root.startAccountCreation(userCobra.Admin)
+		//root.startAccountCreation(userCobra.Admin)
+
+		if userCobra.ApplyPolicy {
+			fmt.Println(root.config.Policy)
+			policyStr := root.config.Policy.BuildCommand()
+
+			fmt.Println(policyStr)
+		}
 	},
 }
 
 func InitializeUserCmd() {
 	userCmd.Flags().BoolVarP(&userCobra.Admin, "admin", "a", false, "Grants admin to the user")
-	userCmd.Flags().BoolVar(&userCobra.ApplyPolicy, "apply-policy", false, "Applies a password reset policy on login")
+	userCmd.Flags().BoolVar(&userCobra.ApplyPolicy, "policy", false, "Applies a password reset policy on login")
 	userCmd.Flags().BoolVarP(&userCobra.logvars.Verbose, "verbose", "v", false, "Enables info logging")
 	userCmd.Flags().BoolVar(&userCobra.logvars.Debug, "debug", false, "Enables debug and info logging")
 
