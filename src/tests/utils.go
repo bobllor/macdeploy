9a614cdd0f81341c46235bc6d70de1ab8dcb7183
teboc <bobllor@yahoo.com>
2026-03-12 00:06:47 -0500

    added Checkf for fatal formatting

--- src/tests/utils.go
+++ src/tests/utils.go
@@ -12,3 +12,11 @@ func Fatal(t *testing.T, err error, msg string) {
 		t.Fatal(msg)
 	}
 }
+
+// Checkf uses a FailNow with the format string as its message by
+// checking if the status is true. If status is false, then do nothing.
+func Checkf(t *testing.T, status bool, format string, v ...any) {
+	if status {
+		t.Fatalf(format, v...)
+	}
+}
