6d1c28c5d2c38b18a79d6963af592c3d8d4ae5c6
teboc <bobllor@yahoo.com>
2026-03-12 00:19:01 -0500

    removed projectname from Metadata, updated struct, added ToString

--- src/deploy-files/utils/metadata.go
+++ src/deploy-files/utils/metadata.go
@@ -1,25 +1,49 @@
 package utils
 
 import (
+	"fmt"
 	"os"
+	"strings"
 )
 
 type Metadata struct {
-	Home          string
-	ProjectName   string
-	SerialTag     string
+	Home      string
+	SerialTag string
+	Files     Files
+}
+
+type Files struct {
 	ZipFile       string
 	DistDirectory string
 }
 
-func NewMetadata(projectName string, serialTag string, distDirectory string, zipFile string) *Metadata {
+// NewMetadata creates a new metadata information for the project.
+func NewMetadata(serialTag string, distDirectory string, zipFile string) *Metadata {
 	meta := Metadata{
-		Home:          os.Getenv("HOME"),
-		ProjectName:   projectName,
-		SerialTag:     serialTag,
-		ZipFile:       zipFile,
-		DistDirectory: distDirectory,
+		Home:      os.Getenv("HOME"),
+		SerialTag: serialTag,
+		Files: Files{
+			ZipFile:       zipFile,
+			DistDirectory: distDirectory,
+		},
 	}
 
 	return &meta
+}
+
+// ToString returns a string representation of Metadata.
+func (m *Metadata) ToString() string {
+	sep := "|"
+	slc := []string{}
+
+	appf := func(format string, v ...any) {
+		slc = append(slc, fmt.Sprintf(format, v...))
+	}
+
+	appf("Home='%s'", m.Home)
+	appf("Serial tag='%s'", m.SerialTag)
+	appf("Zipfile='%s'", m.Files.ZipFile)
+	appf("Dist folder='%s'", m.Files.DistDirectory)
+
+	return strings.Join(slc, sep)
 }
