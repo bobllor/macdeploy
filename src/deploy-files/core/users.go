87211ced0d0c3979881be563452445fd88563fcd
bobllor <bobllor@yahoo.com>
2026-05-07 17:36:07 -0500

    added methods for admin related tasks

--- src/deploy-files/core/users.go
+++ src/deploy-files/core/users.go
@@ -125,6 +125,96 @@ func (u *UserMaker) DeleteAccount(username string) error {
 	return nil
 }
 
+// GrantAdmin grants the given user admin privileges.
+// The user must exist and is not an admin.
+func (u *UserMaker) GrantAdmin(username string) error {
+	err := u.adminInfo.InitializeSudo()
+	if err != nil {
+		return fmt.Errorf("failed to initialize sudo (%v)", err)
+	}
+
+	stat, err := u.isAdmin(username)
+	if err != nil {
+		return fmt.Errorf("failed to check admin status (%v)", err)
+	}
+	if stat {
+		return fmt.Errorf("user %s is already admin", username)
+	}
+
+	cmd := `
+	#!/usr/bin/env bash
+
+	username=$0
+	dseditgroup -o edit -a "$username" -t user admin
+
+	user_group=$(groups "$username")
+	if [[ "$user_group" =~ "admin" ]]; then
+		echo true
+	else
+		echo false
+	fi
+	`
+
+	b, err := exec.Command("sudo", "bash", "-c", cmd, username).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to grant admin (%v)", err)
+	}
+
+	out := strings.TrimSpace(string(b))
+	u.log.Debugf("Grant admin output: %s", out)
+
+	if out == "false" {
+		return fmt.Errorf("failed to grant admin to user %s", username)
+	}
+
+	return nil
+}
+
+// RevokeAdmin revokes the admin privileges from the user.
+// The user must exist and is an admin.
+func (u *UserMaker) RevokeAdmin(username string) error {
+	err := u.adminInfo.InitializeSudo()
+	if err != nil {
+		return fmt.Errorf("failed to initialize sudo (%v)", err)
+	}
+
+	stat, err := u.isAdmin(username)
+	if err != nil {
+		return fmt.Errorf("failed to check admin status (%v)", err)
+	}
+	if !stat {
+		return fmt.Errorf("user %s is already not admin", username)
+	}
+
+	cmd := `
+	#!/usr/bin/env bash
+
+	username=$0
+	dseditgroup -o edit -d "$username" -t user admin
+
+	user_group=$(groups "$username")
+	if [[ "$user_group" =~ "admin" ]]; then
+		echo true
+	else
+		echo false
+	fi
+	`
+
+	b, err := exec.Command("sudo", "bash", "-c", cmd, username).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to revoke admin (%v)", err)
+	}
+
+	out := strings.TrimSpace(string(b))
+	u.log.Debugf("Revoke admin output: %s", out)
+
+	if out == "true" {
+		return fmt.Errorf("failed to revoke admin to user %s", username)
+	}
+
+	return nil
+}
+
 // AddPasswordPolicy adds a password policy for the user. This can only be
 // ran after adding the Secure Token to the user.
 //
@@ -142,7 +232,48 @@ func (u *UserMaker) AddPasswordPolicy(username string) error {
 	return nil
 }
 
+// checkAdmin checks if the current user is admin. If it fails to run,
+// it will return an error.
+//
+// The existence of the user is checked in the call and will return an error
+// if they do not exist.
+func (u *UserMaker) isAdmin(username string) (bool, error) {
+	cmd := `
+	#!/usr/bin/env bash
+	username=$0
+
+	user_group=$(groups "$username")
+	if [[ "$user_group" =~ "admin" ]]; then
+		echo true
+	else
+		echo false
+	fi
+	`
+	exist, err := u.userExists(username)
+	if err != nil {
+		return false, err
+	}
+	if !exist {
+		return false, fmt.Errorf("user %s does not exist", username)
+	}
+
+	b, err := exec.Command("sudo", "bash", "-c", cmd, strings.ToLower(username)).CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+
+	out := strings.TrimSpace(string(b))
+	if out == "true" {
+		return true, nil
+	}
+
+	u.log.Debugf("Admin command output: %s", out)
+
+	return false, nil
+}
+
 // userExists checks the Users directory for the given username.
+// This reads from the /Users directory on a MacBook.
 func (u *UserMaker) userExists(username string) (bool, error) {
 	usersPath := "/Users"
 
